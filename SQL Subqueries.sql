a6e8de2ab148adc7e0ea14c95208f4c712e77d0e
Alan <alanwatters27@gmail.com>
2026-01-04 16:32:03 +0100

    SQL Subqueries

---
+++ SQL Subqueries.sql
@@ -0,0 +1,29 @@
+use sakila;
+SELECT
+  COUNT(*) AS number_of_copies
+FROM inventory
+WHERE film_id = (
+  SELECT film_id
+  FROM film
+  WHERE title = 'Hunchback Impossible');
+-----
+SELECT
+  title,
+  length
+FROM film
+WHERE length > (
+  SELECT AVG(length)
+  FROM film)
+ORDER BY length DESC;
+-----
+SELECT
+  first_name,
+  last_name
+FROM actor
+WHERE actor_id IN (
+  SELECT actor_id
+  FROM film_actor
+  WHERE film_id = (
+    SELECT film_id
+    FROM film
+    WHERE title = 'Alone Trip'));
